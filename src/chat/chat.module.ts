c3fdaf19a4c5d00843fcb587bb7f2d2d5502c2ba
Victor Aponte <91540714+VictorAponteParedes@users.noreply.github.com>
2026-01-03 18:35:24 -0300

    feat(chat): Agregar módulo de notificaciones a las importaciones del módulo de chat

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -5,9 +5,13 @@ import { ChatGateway } from './chat.gateway';
 import { ChatController } from './chat.controller';
 import { Chat } from './entities/chat.entity';
 import { Message } from './entities/message.entity';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Chat, Message])],
+    imports: [
+        TypeOrmModule.forFeature([Chat, Message]),
+        NotificationsModule,
+    ],
     providers: [ChatGateway, ChatService],
     controllers: [ChatController],
     exports: [ChatService],
